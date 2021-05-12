c34b737a460c6eaaf2a2ad379fec25de1299c545
Yixian Du <duyixian1234@qq.com>
2021-05-12 14:10:34 +0800

    fetch article

--- fetch.py
+++ fetch.py
@@ -1,9 +1,40 @@
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, Page, ElementHandle
 import asyncio
-from pathlib import Path
+from models import Article, Method, Step
 
 
-async def main():
+async def getIntro(page: Page):
+    element = await page.query_selector(f"#mf-section-0")
+    assert element
+    return await element.inner_text()
+
+
+async def getName(method: ElementHandle):
+    element = await method.query_selector(".mw-headline")
+    if not element:
+        return ""
+    return await element.inner_text()
+
+
+async def generateMethods(page: Page):
+    section = 1
+    while element := await page.query_selector(f"#mf-section-{section}"):
+        yield element
+        section += 1
+
+
+async def generateSteps(section: ElementHandle):
+    step = 1
+    while element := await section.query_selector(f"#step-id-{step:02}"):
+        url = ""
+        if img := await element.query_selector(".image"):
+            url = await img.get_attribute("href")
+        text = await element.inner_text()
+        yield url, text
+        step += 1
+
+
+async def fetch(url: str):
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(
@@ -12,26 +43,25 @@ async def main():
         )
         page = await browser.new_page(viewport=dict(width=1920, height=1080))
         await page.goto(
-            "https://zh.wikihow.com/%E6%89%93%E8%B4%A5%E5%91%A8%E4%B8%80%E6%97%A9%E6%99%A8%E5%BF%A7%E9%83%81%E7%97%87",
+            url,
             wait_until="domcontentloaded",
         )
-        section = 0
-        while element := await page.query_selector(f"#mf-section-{section}"):
-            print(f"{section=}")
-            if not section:
-                await element.screenshot(path=f"section-{section}.png")
-                text = await element.inner_text()
-                Path(f"section-{section}.txt").write_text(text)
-                section += 1
+        intro = await getIntro(page)
+        article = Article(title="aaa", origin=url, intro=intro)
+        async for method in generateMethods(page):
+            name = await getName(method)
+            if not name:
                 continue
-            step_index = 1
-            while step := await element.query_selector(f".step-id-{step_index:02}"):
-                await step.screenshot(path=f"{section}-{step_index}.png")
-                text = await step.inner_text()
-                Path(f"{section}-{step_index}.txt").write_text(text)
-                step_index += 1
-            section += 1
+            new = Method(name=name)
+            async for url, text in generateSteps(method):
+                new.steps.append(Step(pic=url, text=text))
+            article.methods.append(new)
+        print(article)
         await browser.close()
 
 
-asyncio.run(main())
+asyncio.run(
+    fetch(
+        "https://zh.wikihow.com/%E6%89%93%E8%B4%A5%E5%91%A8%E4%B8%80%E6%97%A9%E6%99%A8%E5%BF%A7%E9%83%81%E7%97%87"
+    )
+)
