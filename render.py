5b6d38f3cacc7dce753aa51c0d63d1f0e0b39150
Yixian Du <duyixian1234@qq.com>
2021-05-13 13:31:29 +0800

    add render

---
+++ render.py
@@ -0,0 +1,58 @@
+from models import Approach, Article, Step
+
+
+MARP = """---
+marp: true
+theme: gaia
+paginate: true
+---"""
+
+
+class Render:
+    def __init__(self, article: Article):
+        self.article = article
+        self.md = []
+        self.add_marp().add_title().add_intro().add_approaches()
+
+    def add_marp(self) -> "Render":
+        self.md.append(MARP)
+        return self
+
+    def add_title(self) -> "Render":
+        self.md.append(f"# {self.article.title}")
+        return self.add_description().add_split()
+
+    def add_description(self) -> "Render":
+        self.md.append(f"原文：[{self.article.origin}]({self.article.origin})")
+        return self
+
+    def add_split(self) -> "Render":
+        self.md.append("\n---\n")
+        return self
+
+    def add_intro(self) -> "Render":
+        self.md.append(f"{self.article.intro}")
+        return self.add_split()
+
+    def add_approaches(self) -> "Render":
+        for approach in self.article.approaches:
+            self.add_approach(approach)
+        return self
+
+    def add_approach(self, approach: Approach) -> "Render":
+        self.md.append(f"## {approach.name}")
+        self.add_split()
+        for step in approach.steps:
+            self.add_step(step)
+
+    def add_step(self, step: Step) -> "Render":
+        if step.pic:
+            self.md.append(f"![]({step.pic})")
+            self.add_split()
+        for sub_step in step.sub_steps:
+            self.md.append(sub_step)
+            self.add_split()
+        return self
+
+    def __str__(self):
+        return "\n".join(self.md)
