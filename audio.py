b6ec140ff1e59e2dd2151ba267a4b287730ead34
Yixian Du <duyixian1234@qq.com>
2021-05-15 01:08:31 +0800

    generate audio

---
+++ audio.py
@@ -0,0 +1,21 @@
+import repo
+import utils
+import transform
+from pathlib import Path
+
+
+async def make_audio(title: str):
+    article = await repo.find_article(title)
+    assert article
+    text = article.full_text()
+    index = 0
+    empty = Path("3s.mp3").read_bytes()
+    for paragraph in text.split("\n"):
+        if not paragraph:
+            continue
+        for sentence in utils.tokenize(paragraph):
+            path = Path("audio") / f"{index:03}.mp3"
+            audio = empty if sentence == "---" else transform.transform(sentence)
+            path.write_bytes(audio)
+            print(f"Save {path}")
+            index += 1
--- models.py
+++ models.py
@@ -5,10 +5,19 @@ class Step(BaseModel):
     pic: str
     sub_steps: list[str]
 
+    def full_text(self):
+        return "\n".join(self.sub_steps)
+
 
 class Approach(BaseModel):
     name: str
     steps: list[Step] = Field(default_factory=list)
+
+    def full_text(self):
+        text = self.name
+        text += "\n" + "---" + "\n"
+        text += "\n".join(step.full_text() for step in self.steps)
+        return text
 
 
 class Article(BaseModel):
@@ -17,3 +26,11 @@ class Article(BaseModel):
     origin: str
     intro: str
     approaches: list[Approach] = Field(default_factory=list)
+
+    def full_text(self):
+        text = self.title
+        text += "\n" + "---"
+        text += "\n" + self.intro
+        text += "\n" + "---" + "\n"
+        text += "\n".join(approach.full_text() for approach in self.approaches)
+        return text
